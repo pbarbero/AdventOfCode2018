638016016deda255b325780370062c3ee4e2d3aa
Pilar Barbero Iriarte <pilar.barbero@col.vueling.com>
2018-12-19 17:35:36 +0100

    Init day 6: part one

--- AdventOfCode.UnitTests/Day6Tests.cs
+++ AdventOfCode.UnitTests/Day6Tests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace AdventOfCode.UnitTests
@@ -20,6 +21,15 @@ namespace AdventOfCode.UnitTests
             Assert.True(3 == result);
         }
 
+        [Fact]
+        public void Test3()
+        {
+            var mainCoordinates = BuildMainCoordinates();
+            var coordinate = BuildCoordinate();
+            var result = Coordinator.GetClosestCoordinate(coordinate, mainCoordinates);
+            Assert.True("1" == result);
+        }
+
         private List<string> BuildCoordinates()
         {
             return new List<string>()
@@ -31,6 +41,20 @@ namespace AdventOfCode.UnitTests
                 "5, 5",
                 "8, 9",
             };
+        }
+
+        private List<Tuple<int, int, string>> BuildMainCoordinates()
+        {
+            return new List<Tuple<int, int, string>>()
+            {
+                new Tuple<int, int, string>(1,1, "1"),
+                new Tuple<int, int, string>(1,6, "2"),
+            };
+        }
+
+        private Tuple<int, int> BuildCoordinate()
+        {
+            return new Tuple<int, int>(0, 0);
         }
     }
 }
--- AdventOfCode/Coordinator.cs
+++ AdventOfCode/Coordinator.cs
@@ -1,61 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AdventOfCode
 {
     public static class Coordinator
     {
-        private static List<Tuple<int, int>> MainCoordinates;
-
         public static int GetLargestArea(List<string> coordinates)
         {
-            MainCoordinates = ParseData(coordinates);
-
-            return 17;
+            var mainCoordinates = ParseData(coordinates);
+            var dictionary = BuildDictionary(mainCoordinates);
+            var centrals = SelectCentralCoordinates(mainCoordinates);
+            return dictionary.Where(x => centrals.Contains(x.Key)).Select(x => x.Value.Count()).Max();
         }
 
-        private static string GetClosestCoordinate(Tuple<int, int> coordinate)
+        public static string GetClosestCoordinate(Tuple<int, int> coordinate, List<Tuple<int, int, string>> mainCoordinates)
         {
-            var minDistance = int.MaxValue;
-            var closestCoordinate = string.Empty;
+            var distances = new List<Tuple<string, int>>();
 
-            foreach (var mainCoordinate in MainCoordinates)
+            foreach (var mainCoordinate in mainCoordinates)
             {
                 var distance = GetTaxicabDistance(mainCoordinate.Item1, coordinate.Item1, mainCoordinate.Item2, coordinate.Item2);
 
-                if (distance < minDistance)
-                {
-                    minDistance = distance;
-                    closestCoordinate = ;
-                }
+                distances.Add(new Tuple<string, int>(mainCoordinate.Item3, distance));
             }
 
-            return closestCoordinate;
+            var minDistance = distances.Select(x => x.Item2).Min();
+
+            var distanceCoordinates = distances.Where(x => x.Item2 == minDistance);
+
+            if (distanceCoordinates.Count() == 1)
+            {
+                return distanceCoordinates.First().Item1;
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
-        
+
         public static int GetTaxicabDistance(int x1, int x2, int y1, int y2)
         {
             return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
         }
 
-        private static List<Tuple<int, int>> ParseData(List<string> lines)
+        private static List<Tuple<int, int, string>> ParseData(List<string> lines)
         {
-            var coordinates = new List<Tuple<int, int>>();
+            var coordinates = new List<Tuple<int, int, string>>();
+            var name = 1;
 
             foreach (var line in lines)
             {
                 var splited = line.Split(',');
-                var tuple = new Tuple<int, int>(
-                    Int32.Parse(splited[0].Trim()),
-                    Int32.Parse(splited[1].Trim())
+                var tuple = new Tuple<int, int, string>(
+                    int.Parse(splited[0].Trim()),
+                    int.Parse(splited[1].Trim()),
+                    name.ToString()
                     );
 
                 coordinates.Add(tuple);
+                name++;
             }
 
             return coordinates;
+        }
+
+        private static Dictionary<string, List<Tuple<int, int>>> BuildDictionary(List<Tuple<int, int, string>> mainCoordinates)
+        {
+            var dictionary = new Dictionary<string, List<Tuple<int, int>>>();
+
+            for (int j = 0; j < mainCoordinates.Select(x => x.Item2).Max(); j++)
+            {
+                for (int i = 0; i < mainCoordinates.Select(x => x.Item1).Max(); i++)
+                {
+                    var coordinate = new Tuple<int, int>(i, j);
+                    var closestCoordinate = GetClosestCoordinate(coordinate, mainCoordinates);
+
+                    if (!string.IsNullOrEmpty(closestCoordinate))
+                    {
+                        if (dictionary.ContainsKey(closestCoordinate))
+                        {
+                            dictionary[closestCoordinate].Add(coordinate);
+                        }
+                        else
+                        {
+                            dictionary.Add(closestCoordinate, new List<Tuple<int, int>>() { coordinate });
+                        }
+                    }
+                }
+            }
+
+            return dictionary;
+        }
+
+        private static List<string> SelectCentralCoordinates(List<Tuple<int, int, string>> mainCoordinates)
+        {
+            var centrals = new List<string>();
+            var Xs = mainCoordinates.Select(x => x.Item1);
+            var Ys = mainCoordinates.Select(x => x.Item2);
+
+            foreach (var coordinate in mainCoordinates)
+            {
+                var isCentral = Xs.Any(x => x < coordinate.Item1) && Xs.Any(x => x > coordinate.Item1)
+                             && Ys.Any(y => y < coordinate.Item2) && Ys.Any(y => y > coordinate.Item2);
+
+                if (isCentral)
+                {
+                    centrals.Add(coordinate.Item3);
+                }
+            }
+
+            return centrals;
         }
     }
 }
